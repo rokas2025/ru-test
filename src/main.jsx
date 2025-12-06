857c9a3cda3b64a8bf6845572b7fdbf0955f81da
rokas2025 <rokas@zubas.lt>
2025-12-06 20:34:29 +0100

    fix: Remove non-existent ElevenLabsConversation provider import

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ElevenLabsConversation } from '@elevenlabs/react'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ElevenLabsConversation
-      config={{
-        baseUrl: 'https://api.eu.residency.elevenlabs.io',
-      }}
-    >
-      <App />
-    </ElevenLabsConversation>
+    <App />
   </React.StrictMode>,
 )
